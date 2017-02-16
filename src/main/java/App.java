75f4081f8dff0e9d0901c3adfd4620c684980542
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2017-02-16 00:08:25 -0500

    Step two: multiline list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,8 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Set up github repo. Deploy boilerplate project. Do step 1.");
+        System.out.println("Set up github repo.");
+        System.out.println("Deploy boilerplate project.");
+        System.out.println("Do step 1.");
     }
 }
