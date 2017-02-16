66e65ee0493e2cae27c3cc5be1b1136dac12b819
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2017-02-16 00:04:11 -0500

    Step one: print list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Set up github repo. Deploy boilerplate project. Do step 1.");
     }
 }
